9ff3c8efc1a3cc61bf3d7d6a19c2894134899666
lukaskrevky-hub <lukas.krevky@gmail.com>
2026-04-04 10:16:58 +0200

    Update web.py

--- web.py
+++ web.py
@@ -102,7 +102,7 @@ MENUS = {
 
 # Konfigurační seznamy podporovaných značek pro sekvenční vysílání IR kódů (tzv. "kobercový nálet")
 AVAILABLE_TV_BRANDS = ["tcl", "sony", "samsung"]
-AVAILABLE_AC_BRANDS = ["lg", "daikin", "samsung", "panasonic"]
+AVAILABLE_AC_BRANDS = ["toshiba"]
 AVAILABLE_RADIO_BRANDS = ["auna", "onkyo"]
 AVAILABLE_LED_BRANDS = ["generic_rgb"]
 
